723662f5a23b3021e63545a686b55efbb4434b85
tstansberry <tstansberry00@stuy.edu>
2019-02-04 08:07:43 -0500

    Updated tester code

--- recursion.java
+++ recursion.java
@@ -1,13 +1,69 @@
 import java.util.*;
 
 public class recursion {
-  public static void main(String args[]) {
-    System.out.println(fib(5));
-    System.out.println(fib(10));
+  public static void main(String[] args) {
+    System.out.println("sqrt()");
+    System.out.println("-----------------------\n");
+
+    System.out.println("sqrt(4, 0.00001)");
+    System.out.println("EXPECTED: 2");
+    System.out.println(sqrt(4));         // 2
+
+    System.out.println("----");
+
+    System.out.println("sqrt(8, 0.00001)");
+    System.out.println("EXPECTED: ~2.8284");
+    System.out.println(sqrt(8));         // ~2.8284
+
+    System.out.println("----");
+
+    System.out.println("sqrt(0, 0.00001)");
+    System.out.println("EXPECTED: 0");
+    System.out.println(sqrt(0));         // 0
+
+    System.out.println("\n");
+
+    System.out.println("fib()");
+    System.out.println("-----------------------\n");
+
+    System.out.println("fib(0)");
+    System.out.println("EXPECTED: 0");
+    System.out.println(fib(0));                   // 0
+
+    System.out.println("----");
+
+    System.out.println("fib(1)");
+    System.out.println("EXPECTED: 1");
+    System.out.println(fib(1));                   // 1
+
+    System.out.println("----");
+
+    System.out.println("fib(7)");
+    System.out.println("EXPECTED: 13");
+    System.out.println(fib(7));                   // 13
+
+    System.out.println("makeAllSums()");
+    System.out.println("-----------------------\n");
+
+    System.out.println("makeAllSums(0)");
+    System.out.println("EXPECTED: [0]");
+    System.out.println(makeAllSums(0));           // [0]
+
+    System.out.println("----");
+
+    System.out.println("makeAllSums(1)");
+    System.out.println("EXPECTED: [1, 0]");
+    System.out.println(makeAllSums(1));           // [1, 0]
+
+    System.out.println("----");
+
+    System.out.println("makeAllSums(3)");
+    System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+    System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
   }
   //Functions to calculate square roots
   public static double sqrt(int input) {
-    if (input == 0) throw new IllegalArgumentException();
+    if (input == 0) return 0;
     return sqrtHelper(input, 1);
   }
 
