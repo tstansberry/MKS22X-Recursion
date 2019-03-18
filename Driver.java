95b01ac5b5cc27fbf7efdb394421dcdd73971f01
tstansberry <tstansberry00@stuy.edu>
2019-03-18 09:43:33 -0400

    Fixed idiotic mistakes

---
+++ Driver.java
@@ -0,0 +1,80 @@
+public class Driver {
+  public static void main(String[] args) {
+    testSqrt(4);
+    testFib(5); //5
+    testSqrt(25);
+    testFib(8); //21
+  }
+
+  public static boolean closeEnough(double a, double b){
+    if(a==0.0 && b==0.0)return true;
+    if(a==0.0)return b < 0.00000000001;
+    if(b==0.0)return a < 0.00000000001;
+    return Math.abs(a-b)/a < 0.0001;//This is the normal % difference allowed
+
+}
+
+
+//testcase must be a valid index of your input/output array
+public static void testFib(int testcase){
+  recursion r = new recursion();
+  int[] input = {0,1,2,3,5,30};
+  int[] output ={0,1,1,2,5,832040};
+  int max = input.length;
+  if(testcase < input.length){
+    int in = input[testcase];
+    try{
+
+      int ans = recursion.fib(in);
+      int correct = output[testcase];
+      if(ans == correct){
+        System.out.println("PASS test fib "+in+". "+correct);
+      }
+      else{
+        System.out.println("FAIL test fib"+in+". "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test fib"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
+
+
+//testcase must be a valid index of your input/output array
+public static void testSqrt(int testcase){
+  recursion r = new recursion();
+  double[] input = {0.0,1.0, 2.0, 4.0, 7.0};
+  double[] output = {0.0,1.0,1.4142135623730951,2.0,2.6457513110645907};
+  int max = input.length;
+  if(testcase < input.length){
+    double in = input[testcase];
+    try{
+
+      double ans = recursion.sqrt(in,.00001);
+      double correct = Math.sqrt(in);
+      if(closeEnough(ans,correct)){
+        System.out.println("PASS test sqrt "+in+" "+ans);
+      }
+      else{
+        System.out.println("FAIL test sqrt "+in+" "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test sqrt"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
+}
--- recursion.java
+++ recursion.java
@@ -1,76 +1,16 @@
 import java.util.*;
 
 public class recursion {
-  public static void main(String[] args) {
-    System.out.println("sqrt()");
-    System.out.println("-----------------------\n");
-
-    System.out.println("sqrt(4, 0.00001)");
-    System.out.println("EXPECTED: 2");
-    System.out.println(sqrt(4));         // 2
-
-    System.out.println("----");
-
-    System.out.println("sqrt(8, 0.00001)");
-    System.out.println("EXPECTED: ~2.8284");
-    System.out.println(sqrt(8));         // ~2.8284
-
-    System.out.println("----");
-
-    System.out.println("sqrt(0, 0.00001)");
-    System.out.println("EXPECTED: 0");
-    System.out.println(sqrt(0));         // 0
-
-    System.out.println("\n");
-
-    System.out.println("fib()");
-    System.out.println("-----------------------\n");
-
-    System.out.println("fib(0)");
-    System.out.println("EXPECTED: 0");
-    System.out.println(fib(0));                   // 0
-
-    System.out.println("----");
-
-    System.out.println("fib(1)");
-    System.out.println("EXPECTED: 1");
-    System.out.println(fib(1));                   // 1
-
-    System.out.println("----");
-
-    System.out.println("fib(7)");
-    System.out.println("EXPECTED: 13");
-    System.out.println(fib(7));                   // 13
-
-    System.out.println("makeAllSums()");
-    System.out.println("-----------------------\n");
-
-    System.out.println("makeAllSums(0)");
-    System.out.println("EXPECTED: [0]");
-    System.out.println(makeAllSums(0));           // [0]
-
-    System.out.println("----");
-
-    System.out.println("makeAllSums(1)");
-    System.out.println("EXPECTED: [1, 0]");
-    System.out.println(makeAllSums(1));           // [1, 0]
-
-    System.out.println("----");
-
-    System.out.println("makeAllSums(3)");
-    System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
-    System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
-  }
   //Functions to calculate square roots
-  public static double sqrt(int input) {
+  public static double sqrt(double input, double tolerance) {
     if (input == 0) return 0;
-    return sqrtHelper(input, 1);
+    return sqrtHelper(input, tolerance, 1);
   }
 
-  public static double sqrtHelper(int input, double guess) {
-    if (Math.abs((guess * guess - input) / input) <= 0.00001) return guess;
+  public static double sqrtHelper(double input, double tolerance, double guess) {
+    if (Math.abs((guess * guess - input) / input) <= tolerance * input) return guess;
     guess =  (input / guess + guess) / 2;
-    return sqrtHelper(input, guess);
+    return sqrtHelper(input, tolerance, guess);
   }
 
   //Function(s) to calculate all possible sums
